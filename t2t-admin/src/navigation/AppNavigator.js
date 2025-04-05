6f9e378cbf696ed48b1105a10e06fb4ec0647b4b
SiddhBharucha <siddhbharucha@gmail.com>
2025-04-04 22:17:56 -0500

    Modified AppNavigator to include new Reminders and Settings screens.

--- t2t-admin/src/navigation/AppNavigator.js
+++ t2t-admin/src/navigation/AppNavigator.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
- import LoginScreen from '../screens/LoginScreen';
- import RegisterScreen from '../screens/RegisterScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
 import CalendarScreen from '../screens/CalendarScreen';
 import { AuthContext } from '../context/AuthContext';
 import NotificationsScreen from '../screens/NotificationsScreen';
+import RemindersScreen from '../screens/RemindersScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
@@ -15,28 +17,39 @@ const Tab = createBottomTabNavigator();
 
 
 const TabNavigator = () => (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Notifications"
-        component={NotificationsScreen}
-        options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="notifications-outline" color={color} size={size} />),
-        }}
-      />
-       <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="home-outline" color={color} size={size} />),
-        }}
-      />
-      <Tab.Screen
-        name="Events"
-        component={CalendarScreen}
-        options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="calendar-outline" color={color} size={size} /> ),
-        }}
-      />
-     
-    </Tab.Navigator>
-  );
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Notifications"
+      component={NotificationsScreen}
+      options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="notifications-outline" color={color} size={size} />),
+      }}
+    />
+     <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="home-outline" color={color} size={size} />),
+      }}
+    />
+    <Tab.Screen
+      name="Events"
+      component={CalendarScreen}
+      options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="calendar-outline" color={color} size={size} /> ),
+      }}
+    />
+    <Tab.Screen
+      name="Reminders"
+      component={RemindersScreen}
+      options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="alarm-outline" color={color} size={size} /> ),
+      }}
+    />
+    <Tab.Screen
+      name="Settings"
+      component={SettingsScreen}
+      options={{headerShown: true, tabBarIcon: ({ color, size }) => (<Icon name="settings-outline" color={color} size={size} /> ),
+      }}
+    />
+  </Tab.Navigator>
+);
 
 const AppNavigator = () => {
     const { isAuthenticated } = useContext(AuthContext);
